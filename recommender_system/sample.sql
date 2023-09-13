030f99fb46dc46fa02471bf778e8f7b92db9c455
Sai <sailkk001@mymail.sim.edu.sg>
2023-09-13 20:54:21 +0800

    database testing

---
+++ recommender_system/sample.sql
@@ -0,0 +1,10 @@
+-- INSERT INTO main_product (product_id, name, description, product_owner_id)
+-- VALUES (123, "testing", "testing" , 1),
+--         (124, "testing2", "testing2",1);
+
+
+SELECT * FROM main_product;
+
+-- DELETE FROM main_product WHERE product_id > 100;
+
+-- SELECT * FROM main_product;
