9cf3c7d14f2b5901d224bdb29d4470fc11f2f2f5
Sai <sailkk001@mymail.sim.edu.sg>
2023-09-16 22:15:11 +0800

    Reviews

--- recommender_system/main/urls.py
+++ recommender_system/main/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('home', views.home, name = 'home'),
     path('sign-up', views.sign_up, name = 'sign_up'),
     path('add-product', views.add_product, name = 'add_product'),
-    path('product/<int:product_id>/', views.product_detail, name='product_detail')
+    path('product/<int:product_id>/', views.product_detail, name='product_detail'),
 ]
