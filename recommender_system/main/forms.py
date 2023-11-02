d59aa8fd0f2a1070401026b19af2ba36d69a3558
SaiLaoKhayKham <sailkk001@mymail.sim.edu.sg>
2023-11-02 08:56:49 +0800

    View, Edit, Update, Delete Profile(user)

--- recommender_system/main/forms.py
+++ recommender_system/main/forms.py
@@ -2,7 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Product, Review, CustomUser
-
+from django.contrib.auth.forms import UserChangeForm
+from django.urls import reverse
 class RegisterForm(UserCreationForm):
     email = forms.EmailField(required = True)
     name = forms.CharField(max_length = 100)
@@ -22,6 +23,18 @@ class RegisterForm(UserCreationForm):
         model = User
         fields = ['username', 'first_name', 'last_name', 'gender', 'email', 'password1', 'password2']
 
+
+
+
+
+class UserProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email']
+        
+    
+    
+   
 
 
 class ProductForm(forms.ModelForm):
--- recommender_system/main/urls.py
+++ recommender_system/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.home, name = 'home'),
@@ -9,5 +10,19 @@ urlpatterns = [
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('import/', views.import_from_excel, name='import_from_excel'),
     path('search/', views.search_products, name='search_products'),
+    
+    
+    path('edit_account/', views.edit_account, name='edit_account'),
+    path('view_account/', views.view_account, name='view_account'),
+    path('change_password',views.change_password,name='change_password'),
+    path('password_change_done',views.PasswordChangeDoneView.as_view(), name= 'password_change_done'),  
+    path('delete_account',views.delete_account, name = 'delete_account'),  
 
+    
+    
+    
 ]
+
+
+
+
